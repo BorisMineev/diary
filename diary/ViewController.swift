f4a6881a3b0522323aa3131ab91996bcc317a8b2
Glip <Glip@MacBook-Air-Mac.local>
2021-03-01 07:18:02 +0400

    first commit

---
+++ diary/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  diary
+//
+//  Created by Glip on 28.02.2021.
+//
+
+import UIKit
+import JTAppleCalendar
+import EventKit
+import EventKitUI
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: JTACMonthViewDataSource {
+    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        let startDate = formatter.date(from: "2021 01 01")!
+        let endDate = Date()
+        return ConfigurationParameters(startDate: startDate, endDate: endDate)
+    }
+    }
+extension ViewController: JTACMonthViewDelegate {
+    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = cell as! dateCell
+        cell.dateLable.text = cellState.text
+    }
+    
+    func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! dateCell
+        cell.dateLable.text = cellState.text
+        return cell
+    }
+    
+    
+}
+//extension ViewController: JTACMonthViewDelegate, JTACMonthViewDataSource {
+//    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "yyyy MM dd"
+//        formatter.timeZone = Calendar.current.timeZone
+//        formatter.locale = Calendar.current.locale
+//
+//        let startDate = formatter.date(from: "2021 01 01")!
+//        let endDate = formatter.date(from: "2021 12 31")!
+//
+//        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+//        return parameters
+//    }
+//
+//    func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
+//        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+//        cell.dateLabel.text = cellState.text
+//        return cell
+//    }
+//    
+//    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+//        let cell = cell as! CustomCell
+//        cell.dateLabel.text = cellState.text
+//    }
+//}
---
+++ diary/dateCell.swift
@@ -0,0 +1,13 @@
+//
+//  dateCell.swift
+//  diary
+//
+//  Created by Glip on 01.03.2021.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class dateCell: JTACDayCell {
+    @IBOutlet var dateLable: UILabel!
+}
